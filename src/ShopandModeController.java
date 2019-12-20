e984bdc6b8a55d612fa20dbd33405c502d76012d
leylaHashimliii <56173558+leylaHashimliii@users.noreply.github.com>
2019-12-20 03:41:35 +0300

    Add files via upload

--- src/ShopandModeController.java
+++ src/ShopandModeController.java
@@ -28,32 +28,10 @@ public class ShopandModeController extends UserMenuController
     private Label information;
     @FXML
     private Button buyOrChoose;
-    @FXML
-    private RadioButton marvel;
-    @FXML
-    private ToggleGroup toggleGroup;
-    @FXML
-    private RadioButton dc;
-    @FXML
-    private void initialize(){
-        toggleGroup = new ToggleGroup();
-        marvel.setToggleGroup(toggleGroup);
-        marvel.setSelected(true);
-        dc.setToggleGroup(toggleGroup);
-    }
-    public void onModeSelected(ActionEvent event) throws Exception{
-        RadioButton selectedMode = (RadioButton) toggleGroup.getSelectedToggle();
-        String value = selectedMode.getText();
-        System.out.println(value);
-        Stage window = (Stage) marvel.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getResource("UserMenu.fxml"));
-        window.setScene(new Scene(root));
-        
-        // TODO: return the value of the selected Radio Button
-    }
 
     private ArrayList<String> informationsC;
     private ArrayList<String> informationsW;
+    private Player player;
     Boolean isWeapon;
     public void fromStoreToUserMenu(ActionEvent event)throws Exception{
         System.out.println("From controller");
@@ -67,6 +45,11 @@ public class ShopandModeController extends UserMenuController
         System.out.println("Here");
         this.informationsC = informationsC;
         this.informationsW = informationsW;
+    }
+
+    public void setPlayer( Player player)
+    {
+        this.player = player;
     }
 
     public void goBackToUserMenu(ActionEvent event)throws Exception{
@@ -101,13 +84,77 @@ public class ShopandModeController extends UserMenuController
 
     public void buyStuff(ActionEvent event)throws Exception{
         System.out.println("buy stuff or choose stuff");
-        if( !isWeapon)
+        if( !isWeapon )
         {
-            System.out.println(images.getSelectionModel().getSelectedIndex());
+            int a = information.getText().indexOf("price");
+            String foo = information.getText().substring(a + 6);
+            System.out.println(foo);
+            //int price = Integer.parseInt(foo);
+            int price = 0;
+            if( player.getCoins() > price)
+            {
+                Character temp = new Character();
+                temp.setImage(images.getSelectionModel().getSelectedItem().getImage());
+                int i = information.getText().indexOf("price"); // 4
+                String word = information.getText().substring(0, i);
+                temp.setName(word);
+                player.addCharacter(temp);
+                System.out.println(images.getSelectionModel().getSelectedIndex());
+                System.out.println("aldiq");
+            }
         }
         else
         {
-            System.out.println(images1.getSelectionModel().getSelectedIndex());
+            int a = information.getText().indexOf("price"); // 4
+            String foo1 = information.getText().substring(a + 6);
+            System.out.println(foo1);
+            int price = Integer.parseInt(foo1);
+            if( player.getCoins() > price)
+            {
+                Weapon temp = new Weapon();
+                temp.setImage(images1.getSelectionModel().getSelectedItem().getImage());
+                int i = information.getText().indexOf("price"); // 4
+                String word1 = information.getText().substring(0, i);
+                temp.setName(word1);
+                player.addWeapon(temp);
+                System.out.println(images1.getSelectionModel().getSelectedIndex());
+                System.out.println("aldiq");
+            }
+
         }
+    }
+
+    public void chooseStuff(ActionEvent event) throws Exception
+    {
+        int i = information.getText().indexOf("price"); // 4
+        Boolean found = false;
+        String word = information.getText().substring(0, i);
+        if( !isWeapon && !player.getCharacters().isEmpty())
+        {
+            for(int a = 0; a < player.getCharacters().size(); a++)
+            {
+                if(player.getCharacters().get(a).getName().equals(word) )
+                {
+                    player.setCurrentCharacter(player.getCharacters().get(a));
+                    found = true;
+                }
+            }
+        }
+        else if(!player.getCharacters().isEmpty())
+        {
+            for(int a = 0; a < player.getWeapons().size(); a++)
+            {
+                if(player.getWeapons().get(a).getName().equals(word) )
+                {
+                    player.setCurrentWeapon(player.getWeapons().get(a));
+                    found = true;
+                }
+            }
+        }
+        if(!found)
+        {
+            information.setText("Not in your collection!");
+        }
+        System.out.println(found);
     }
 }
