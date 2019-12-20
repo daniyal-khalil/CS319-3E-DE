0d86c4409608349521480fcc5c986d66796a4287
Daniyal Khalil <55948079+daniyal-khalil@users.noreply.github.com>
2019-12-21 01:41:01 +0300

    Commit

--- src/GameOverController.java
+++ src/GameOverController.java
@@ -1,8 +1,30 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class GameOverController {
+
+    @FXML
+    private Label scoreLabel;
+    private Player player;
+    private int Score;
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+
+    public void setScoreLabel(int score) {
+
+        scoreLabel.setText(score + "");
+        Saver saver = new Saver("src\\sample\\a.txt");
+        saver.updateScore(player.getName(), score);
+    }
 
 
     public void goingToPlay(ActionEvent event) {
@@ -10,4 +32,5 @@ public class GameOverController {
 
     public void exittingApp(ActionEvent event) {
     }
+
 }
