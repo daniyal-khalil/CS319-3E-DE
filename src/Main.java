9ba4665ef8c7663a1320d98a1e14c8988a746411
leylaIsmayilova <56220811+leylaIsmayilova@users.noreply.github.com>
2019-12-20 17:57:53 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("StartUpMenu.fxml"));
+        Audio music = new Audio("C:\\Users\\Asus\\Desktop\\starman.mp3");
+        Audio gameAudio = new Audio("C:\\Users\\Asus\\Desktop\\starman.mp3");
+
+        FXMLLoader loader =new  FXMLLoader(getClass().getResource("StartUpMenu.fxml"));
+        Parent root = (Parent) loader.load();
+
+        StartUpMenuController startUpMenuController = new StartUpMenuController();
+        startUpMenuController = loader.getController();
+        System.out.println("passed to first scene");
+        startUpMenuController.setAudioEffect(gameAudio);
+        startUpMenuController.setMusic(music);
+        music.play();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1024, 720));
         primaryStage.show();
