f946339d6710c015bb68943d5264e91b79080c5b
Leyla Ismayilova <56220811+leylaIsmayilova@users.noreply.github.com>
2019-12-21 14:03:55 -0800

    Update StartUpMenuController.java

--- src/StartUpMenuController.java
+++ src/StartUpMenuController.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.image.ImageView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
