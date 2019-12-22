5175f6c208580e8766195158c28a4bf5fc647433
Verdiyev Zulfugar <39241206+Verdiyev@users.noreply.github.com>
2019-12-23 00:56:44 +0300

    Add files via upload

---
+++ GameOverController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class GameOverController {
+
+
+    public void goingToPlay(ActionEvent event) {
+    }
+
+    public void exittingApp(ActionEvent event) {
+    }
+}
